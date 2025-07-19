ff49a1609a567a1a64402a57c895af54abd3195d
noname9921 <baobao1102@gmail.com>
2025-07-19 20:20:34 +0700

    fix static file serving

--- Backend/backend.js
+++ Backend/backend.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '../Frontend')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
