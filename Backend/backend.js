eda07b11df765646c92ab4740a3436b5f20f21d3
noname9921 <baobao1102@gmail.com>
2025-07-19 20:08:51 +0700

    Ready for Render deploy

--- Backend/backend.js
+++ Backend/backend.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
