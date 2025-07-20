2d409ebe63db3daa74f0ab09ed2cc83640dd480b
noname9921 <baobao1102@gmail.com>
2025-07-20 16:26:16 +0700

    update

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,13 @@
 const cardwrapper = document.querySelector('.card-wrapper');
 const cardheader = document.querySelector('.cardheader');
+const cardcontent = document.querySelector('.cardcontent');
 const card = document.querySelector('.card');
-cardheader.addEventListener('click', () => {
+card.addEventListener('click', () => {
     card.classList.toggle('open');
     cardwrapper.classList.toggle('open');
-    updateSpinDuration(cardwrapper);
     if (window.MathJax && MathJax.typeset) {
         MathJax.typeset([card]);
     }
 });
+
 
